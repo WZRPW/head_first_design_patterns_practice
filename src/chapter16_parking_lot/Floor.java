398a96accf5a8e5ded857c713be7ca026e7079e1
Iamthegeekdavid <wei.zorro@gmail.com>
2019-01-06 23:34:05 -0600

    Design simple parking lot- finished the floor class

--- src/chapter16_parking_lot/Floor.java
+++ src/chapter16_parking_lot/Floor.java
@@ -1,4 +1,52 @@
 package chapter16_parking_lot;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Floor {
+    private final List<ParkingSpot> spots;
+
+    Floor(int numOfSpots) {
+        List<ParkingSpot> list = new ArrayList<>(numOfSpots);
+        int i = 0;
+         for (; i < numOfSpots / 2; i++) {
+             list.add(new ParkingSpot(VehicleSize.Compact));
+         }
+
+         for (; i < numOfSpots; i++) {
+             list.add(new ParkingSpot(VehicleSize.Large));
+         }
+
+         spots = Collections.unmodifiableList(list);
+    }
+
+    boolean hasSpot(Vehicle v) {
+        for (ParkingSpot s: spots) {
+            if (s.fit(v)) { return true; }
+        }
+        return false;
+    }
+
+    boolean park(Vehicle v) {
+        for (ParkingSpot s : spots) {
+            if (s.fit(v)) {
+                s.park(v);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    boolean leave(Vehicle v) {
+        for (ParkingSpot s : spots) {
+            if (s.getVehicle() == v) {
+                s.leave();
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
