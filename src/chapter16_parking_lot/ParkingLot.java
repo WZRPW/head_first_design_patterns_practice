940d9167206af64802f666df1f51f178d781f3b7
Iamthegeekdavid <wei.zorro@gmail.com>
2019-01-09 23:39:30 -0600

    Final part of Parking Lot project

--- src/chapter16_parking_lot/ParkingLot.java
+++ src/chapter16_parking_lot/ParkingLot.java
@@ -1,4 +1,40 @@
 package chapter16_parking_lot;
 
 public class ParkingLot {
+
+    private final Floor[] floors;
+
+    public ParkingLot(int numFloors, int numSpotsPerFloor) {
+        floors = new Floor[numFloors];
+        for (int i = 0; i < numFloors; i++) {
+            floors[i] = new Floor(numSpotsPerFloor);
+        }
+    }
+
+    public boolean hasSpot(Vehicle v) {
+        for (Floor Flr : floors) {
+            if (Flr.hasSpot(v)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean park(Vehicle v) {
+        for(Floor flr : floors) {
+            if (flr.park(v)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean leave(Vehicle v) {
+        for (Floor flr : floors) {
+            if (flr.leave(v)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
