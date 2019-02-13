978d04dc2aa36145ac807c9e964afc14fa1fb5c3
Iamthegeekdavid <wei.zorro@gmail.com>
2019-02-13 16:41:00 -0600

    Composite Pattern - menu prj of test drive class

---
+++ src/chapter9_iterator_and_composite/menu/MenuTestDrive.java
@@ -0,0 +1,76 @@
+package chapter9_iterator_and_composite.menu;
+
+public class MenuTestDrive {
+    public static void main(String[] args) {
+        MenuComponent pancakeHouseMenu =
+                new Menu("PANCAKE HOUSE MENU", "Breakfast");
+        MenuComponent dinerMenu =
+                new Menu("DINER MENU", "Lunch");
+        MenuComponent cafeMenu =
+                new Menu("CAFE MENU", "Dinner");
+        MenuComponent dessertMenu =
+                new Menu("DESSERT MENU", "Dessert of course!");
+        MenuComponent coffeeMenu = new Menu("COFFEE MENU", "Stuff to go with your afternoon coffee");
+
+        MenuComponent allMenus = new Menu("ALL MENUS", "All menus combined");
+
+        allMenus.add(pancakeHouseMenu);
+        allMenus.add(dinerMenu);
+        allMenus.add(cafeMenu);
+
+        pancakeHouseMenu.add(new MenuItem(
+                "K&B's Pancake Breakfast",
+                "Pancakes with scrambled eggs, and toast",
+                true,
+                2.99));
+        pancakeHouseMenu.add(new MenuItem(
+                "Regular Pancake Breakfast",
+                "Pancakes with fried eggs, sausage",
+                false,
+                2.99));
+        pancakeHouseMenu.add(new MenuItem(
+                "Blueberry Pancakes",
+                "Pancakes made with fresh blueberries, and blueberry syrup",
+                true,
+                3.49));
+        pancakeHouseMenu.add(new MenuItem(
+                "Waffles",
+                "Waffles, with your choice of blueberries or strawberries",
+                true,
+                3.59));
+
+        dinerMenu.add(new MenuItem(
+                "Vegetarian BLT",
+                "(Fakin') Bacon with lettuce & tomato on whole wheat",
+                true,
+                2.99));
+        dinerMenu.add(new MenuItem(
+                "BLT",
+                "Bacon with lettuce & tomato on whole wheat",
+                false,
+                2.99));
+        dinerMenu.add(new MenuItem(
+                "Soup of the day",
+                "A bowl of the soup of the day, with a side of potato salad",
+                false,
+                3.29));
+        dinerMenu.add(new MenuItem(
+                "Hotdog",
+                "A hot dog, with saurkraut, relish, onions, topped with cheese",
+                false,
+                3.05));
+        dinerMenu.add(new MenuItem(
+                "Steamed Veggies and Brown Rice",
+                "Steamed vegetables over brown rice",
+                true,
+                3.99));
+
+        dinerMenu.add(new MenuItem(
+                "Pasta",
+                "Spaghetti with Marinara Sauce, and a slice of sourdough bread",
+                true,
+                3.89));
+
+        dinerMenu.add(dessertMenu);
+    }
+}
