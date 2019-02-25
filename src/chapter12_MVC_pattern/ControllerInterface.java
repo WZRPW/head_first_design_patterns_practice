c67e77c60261d19fc314f6a41f800ddcb8d09339
Iamthegeekdavid <wei.zorro@gmail.com>
2019-02-24 23:18:56 -0600

    Chapter 12 - MVC controller Interface

---
+++ src/chapter12_MVC_pattern/ControllerInterface.java
@@ -0,0 +1,9 @@
+package chapter12_MVC_pattern;
+
+public interface ControllerInterface {
+    void start();
+    void stop();
+    void increaseBPM();
+    void decreaseBPM();
+    void setBPM(int bpm);
+}
