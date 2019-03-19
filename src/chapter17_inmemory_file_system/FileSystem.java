74f44b786349cace18623a22c0167eec93ba9ac4
Iamthegeekdavid <wei.zorro@gmail.com>
2019-03-18 23:04:23 -0500

    Chapter17 File System class (not done yet)

--- src/chapter17_inmemory_file_system/FileSystem.java
+++ src/chapter17_inmemory_file_system/FileSystem.java
@@ -1,6 +1,32 @@
 package chapter17_inmemory_file_system;
 
+//1. File System class not done yet
+import java.util.*;
+import java.lang.String;
+
 public class FileSystem {
-    private File myFile;
-    private Directory myDirectory;
+
+    private final Directory root;
+    public FileSystem() { root = new Directory("/", null); }
+
+    public List<Entry> resolve(String path) {
+        assert path.startsWith("/");
+        String[] components = path.substring(1).split("/");
+        List<Entry> entries = new ArrayList<Entry>(components.length + 1);
+        entries.add(root);
+
+        Entry entry = root;
+        for (String component : components) {
+            if (entry == null || !(entry instanceof Directory)) {
+                throw new IllegalArgumentException("invalid path : " + path);
+            }
+            if (!component.isEmpty()) {
+                entry = (Directory)entry.getChild(component);
+                entries.add(entry);
+            }
+            return entries;
+        }
+    }
+
+
 }
